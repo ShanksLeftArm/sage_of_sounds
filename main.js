4e3043b0a9e3de1554e271df9622f664a7e795e3
ShanksLeftArm <shanks.left.arm44@gmail.com>
2022-08-24 01:54:34 -0400

    Queue functionality added but bugging out on transition

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const Discord = require('discord.js');
+const Client = Discord.Client;
+const GatewayIntentBits = Discord.GatewayIntentBits;
 const { token } = require('./config.json')
 const fs = require('fs');
 
+const partials = [ "MESSAGE", "CHANNEL", "REACTION" ];
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates], partials: partials});
 const botName = 'SageOfSounds';
 const prefix = '*';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildVoiceStates]});
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-var commands = {};
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-
-    commands[command.name] =  command;
-}
-
-client.once('ready', () => {
+client.once('ready', ()=> {
     console.log('SageOfSounds in online...');
+    client.application.commands = new Discord.Collection();
+    client.application.events = new Discord.Collection();
+
+    ['command_handler', 'event_handler'].forEach(handler =>{
+        require(`./handlers/${handler}`)(client, Discord);
+    })
 });
 
-client.on('messageCreate', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift();
-    const commandID = command.toLowerCase();
-    if (commandID in commands) {
-        commands[commandID].execute(message, args);
-    } else {
-        console.log(`Command \'${command}\' is not known to ${botName}`);
-        return message.reply(`Your instruction of \'${command}\' is not something I know how to do`);
-    }
-})
+// client.on('messageCreate', message => {
+//     if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+//     const args = message.content.slice(prefix.length).split(/ +/);
+//     const command = args.shift();
+//     const commandID = command.toLowerCase();
+//     if (commandID in commands) {
+//         commands[commandID].execute(message, args);
+//     } else {
+//         console.log(`Command \'${command}\' is not known to ${botName}`);
+//         return message.reply(`Your instruction of \'${command}\' is not something I know how to do`);
+//     }
+// })
 
 client.login(token);
